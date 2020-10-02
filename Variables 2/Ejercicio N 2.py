4479f5360d3960beb65cc6383a8a629631442115
PaoloVelarde7544 <paolo.velarde@ucb.edu.bo>
2020-10-01 16:29:27 -1200

    Ejercicio 2 de variables 2

---
+++ Variables 2/Ejercicio N 2.py
@@ -0,0 +1,5 @@
+dividendo = int(input("ingresar numero"))
+divisor = int(input("ingresar el divisor"))
+respuesta = dividendo % divisor
+respuesta = f"El resto obtenido al dividir {dividendo} entre {divisor} es {respuesta}"
+print(respuesta)
