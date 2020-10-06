dd3d2841a7cc92eae53a8dc6b37ecb3e4e21d6b6
PaoloVelarde7544 <paolo.velarde@ucb.edu.bo>
2020-10-05 15:02:38 -1200

    Flujo condicional Ejercicio 5

---
+++ Flujo condicional/Ejercicio 5.py
@@ -0,0 +1,14 @@
+num = int(input("Ingrese un numero: "))
+if num !=0 :
+    print("Es distinto a 0 ")
+    if num <0 :
+        print("Es negativo")
+    else:
+        print("Es positivo")
+else :
+    print("Es cero")
+
+
+
+
+
