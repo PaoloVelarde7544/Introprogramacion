730f7d8c89ce8a74e1aeffc8c7bd8bf84daa9e24
PaoloVelarde7544 <paolo.velarde@ucb.edu.bo>
2020-10-13 10:59:52 -1200

    Ejercicio_9_Calculadora Basica

---
+++ Practico_1/Ejercicio 9.py
@@ -0,0 +1,16 @@
+num1 = int(input("operando: "))
+signo = input("operando: ")
+num2 = int(input("Operando: "))
+if num1+num2 :
+    print(f"{num1} {signo} {num2}={num1 + num2}")
+elif num1-num2 :
+    print(f"{num1} {signo} {num2}={num1 - num2}")
+elif num1%num2 :
+    print(f"{num1} {signo} {num2}={num1 % num2}")
+elif num1*num2 :
+    print(f"{num1} {signo} {num2}={num1 * num2}")
+elif num1**num2 :
+    print(f"{num1} {signo} {num2}={num1 ** num2}")
+
+
+
