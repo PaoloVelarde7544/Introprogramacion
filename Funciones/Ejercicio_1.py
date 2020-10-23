8251ef20ae08f624438ef31c38f18a45277447fa
PaoloVelarde7544 <paolo.velarde@ucb.edu.bo>
2020-10-22 16:47:14 -1200

    Ejercicios Tablas

---
+++ Funciones/Ejercicio_1.py
@@ -0,0 +1,12 @@
+#Tablas de multiplicar
+def tablaM(numi):
+    for i in range(1, 11):
+        print(f" {numi} x {i} = {i * numi}")
+
+for j in range(1,11):
+    print("tabla del : ",j)
+    tablaM(j)
+
+
+
+
